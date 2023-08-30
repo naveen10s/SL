e5fbe558f169fd25538c72dba07fbdaf437ebf3a
naveen10s <naveens10102002@gmail.com>
2023-08-30 14:49:33 +0530

    student database

---
+++ project_library/project_library/doctype/student_database/student_database.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Naveen and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Student Database', {
+	// refresh: function(frm) {
+
+	// }
+});
